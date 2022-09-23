687a34e66a01d8d86b38ac561e13835dc3cb672e
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-09-23 10:29:20 -0600

    remove CSV.parse() try/catch

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,21 +1,22 @@
 import * as CSV from 'papaparse';
+import type {MarkerFileData} from './DataTypes';
 
 
 export async function parseMarkerFileData(file: File): Promise<MarkerFileData> {
+
+    const markersRow = 1;
+    const frame1Row = 5;
+    const timesCol = 1;
+    const marker1Col = 2;
 
     if (!file || !(file.type==="text/plain" || file.type==="text/csv")) {
         return Promise.reject(new Error("Unsupported file type "+file.type));
     }
 
-    let parseResult: CSV.ParseResult<string>|null = null;
-    try {
-        parseResult = await new Promise((resolve, reject) => {
-            CSV.parse(file, {delimiter: "\t", complete: resolve, error: reject});
-        });
-    }
-    catch (err) {
-        return Promise.reject(err);
-    }
+    const parseResult: CSV.ParseResult<string>|null = await new Promise((resolve, reject) => {
+        CSV.parse(file, {delimiter: "\t", complete: resolve, error: reject});
+    });
+
     if (!parseResult || !parseResult.data || parseResult.data.length<6 || parseResult.data[4][0] !== "ITEM") {
         return Promise.reject(new Error("Could not read file as TSV"));
     }
@@ -29,24 +30,24 @@ export async function parseMarkerFileData(file: File): Promise<MarkerFileData> {
 
     /* Read marker labels from row 1 */
     let label = "";
-    for (let col=2; col<tsvTable[1].length; col++) {
-        label = tsvTable[1][col];
+    for (let col=marker1Col; col<tsvTable[markersRow].length; col++) {
+        label = tsvTable[markersRow][col];
         if (!labelToIdxMap.has(label)) {
             labelToIdxMap.set(label,col);
             fileData.markers.push({label: label});
         }
     }
 
-    /* Extract row data: for every row beneath the 5 header rows, */
-    for (let row=5; row<tsvTable.length; row++) {
+    /* Extract row data */
+    for (let row=frame1Row; row<tsvTable.length; row++) {
         /* Read a new Frame */
         fileData.frames.push({
-            time: parseFloat(tsvTable[row][1]),
+            time: parseFloat(tsvTable[row][timesCol]),
             positions: []
         });
         /* Populate the Frame's positions */
         labelToIdxMap.forEach(labelIdx => { //iterates through labels in insertion order
-            fileData.frames[row-5].positions.push({
+            fileData.frames[row-frame1Row].positions.push({
                 x: parseFloat(tsvTable[row][labelIdx]),
                 y: parseFloat(tsvTable[row][labelIdx+1]),
                 z: parseFloat(tsvTable[row][labelIdx+2])
