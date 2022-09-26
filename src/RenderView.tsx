674decb10071529e3330bb5ca67e826e84d4ac18
Colin Reeder <colin@vpzom.click>
2022-09-26 16:07:33 -0600

    Initial work on rendering

--- src/RenderView.tsx
+++ src/RenderView.tsx
@@ -1,30 +1,59 @@
 import * as THREE from "three";
 import * as React from "react";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
-export default function RenderView() {
+import { MarkerFileData } from "./DataTypes";
+
+interface Props {
+	data: MarkerFileData;
+	frame: number;
+}
+
+export default function RenderView(props: Props) {
 	const width = 400;
 	const height = 400;
 
 	const [camera] = useState(() => {
 		const result = new THREE.PerspectiveCamera(70, width / height);
-		result.position.z = .5;
+		result.position.z = 1.5;
 		result.position.y = .5;
 		result.rotation.x = -(Math.PI / 4);
 		return result;
 	});
 
 	const [scene] = useState(() => {
-		const result = new THREE.Scene();
-
-		const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-		const material = new THREE.MeshNormalMaterial();
-
-		const mesh = new THREE.Mesh(geometry, material);
-		result.add(mesh);
-
-		return result;
+		return new THREE.Scene();
 	});
+
+	const pointsRep = useMemo(() => {
+		return props.data.markers.map(marker => {
+			const geometry = new THREE.BoxGeometry(0.01, 0.01, 0.01);
+			const material = new THREE.MeshNormalMaterial();
+
+			const mesh = new THREE.Mesh(geometry, material);
+
+			return mesh;
+		});
+	}, [props.data.markers]);
+
+	useEffect(() => {
+		pointsRep.forEach(pointRep => scene.add(pointRep));
+
+		return () => pointsRep.forEach(pointRep => scene.remove(pointRep));
+	}, [scene, pointsRep]);
+
+	useEffect(() => {
+		const frameData = props.data.frames[props.frame];
+		pointsRep.forEach((pointRep, idx) => {
+			const pos = frameData.positions[idx];
+			if(pos === null) return;
+
+			pointRep.position.x = pos.x;
+			pointRep.position.y = pos.y;
+			pointRep.position.z = pos.z;
+		});
+	}, [pointsRep, props.data, props.frame]);
+		
 
 	const [renderer] = useState(() => {
 		const result = new THREE.WebGLRenderer({antialias: true});
