e2af1094d61dcc222c1a12204238f8e453f3f523
PTLux1517 <61641913+PTLux1517@users.noreply.github.com>
2022-10-04 21:29:18 -0600

    implement camera flight controls (#106)
    * implement camera flight controls
    * add useEffect cleanup function for event listeners
    * change keypress handler from using e.key to e.code
    * didn't realize there are also Numpad key codes

--- src/RenderView.tsx
+++ src/RenderView.tsx
@@ -1,8 +1,18 @@
 import * as THREE from "three";
 import * as React from "react";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
 
 import { MarkerFileData } from "./DataTypes";
+
+/* Axis reference */     /* THREE's relation to trial subject */
+//THREE X == COBR -X     (+left/-right)
+//THREE Y == COBR Z      (+up/-down)
+//THREE Z == COBR Y      (+front/-back)
+
+const THREExAxis = new THREE.Vector3(1, 0, 0);
+const THREEyAxis = new THREE.Vector3(0, 1, 0);
+const THREEzAxis = new THREE.Vector3(0, 0, 1);
 
 interface Props {
 	data: MarkerFileData;
@@ -14,16 +24,14 @@ export default function RenderView(props: Props) {
 	const height = 450;
 
 	const [camera] = useState(() => {
-		const result = new THREE.PerspectiveCamera(70, width / height);
-		result.position.x = 1.35; //+subject's right, -subject's left
-		result.position.y = -0.4; //+subject's front, -subject's back
-		result.position.z = 0.9; //+subject's up, -subject's down
-
-		result.rotation.x = -1.5; //tilt (+down, -up)
-		result.rotation.y = 0.9; //yaw (+CW, -CCW)
-		result.rotation.z = Math.PI; //roll (+CCW, -CW) cam @ 0 is upside-down with respect to data
-
-		return result;
+		const cam = new THREE.PerspectiveCamera(70, width / height);
+		cam.position.x = -2.0;
+		cam.position.y = 1.5;
+		cam.position.z = 0.0;
+		cam.rotateOnWorldAxis(THREExAxis, -Math.PI/8);
+		cam.rotateOnWorldAxis(THREEyAxis, -Math.PI/2);
+		cam.rotateOnWorldAxis(THREEzAxis, 0.0);
+		return cam;
 	});
 
 	const [scene] = useState(() => {
@@ -53,9 +61,9 @@ export default function RenderView(props: Props) {
 			const pos = frameData.positions[idx];
 			if(pos === null) return;
 
-			pointRep.position.x = pos.x;
-			pointRep.position.y = pos.y;
-			pointRep.position.z = pos.z;
+			pointRep.position.x = -pos.x; //COBR x-axis is inverted with respect to THREE's
+			pointRep.position.y = pos.z; //COBR z-axis is THREE's y-axis (up direction)
+			pointRep.position.z = pos.y; //COBR y-axis is THREE's z-axis (forward direction)
 		});
 	}, [pointsRep, props.data, props.frame]);
 		
@@ -65,6 +73,76 @@ export default function RenderView(props: Props) {
 		result.setSize(width, height);
 		return result;
 	});
+
+	const [cameraControls] = useState(() => new PointerLockControls(camera, renderer.domElement));
+
+	/* Use camera controls */
+	useEffect(() => {
+		const moveMeters = 0.05;
+		/* Only allow flight while mouse is in visualization area */
+		const mouseEnterVizAreaHandler = () => document.addEventListener('keydown', keyPressHandler, false);
+		const mouseLeaveVizAreaHandler = () => document.removeEventListener('keydown', keyPressHandler, false);
+		/* Flight controls (keys) */
+		const keyPressHandler = (e: KeyboardEvent) => {
+			switch (e.code) {
+				/* Fly left */
+				case "KeyA": //for right-handed mouse use
+				case "Digit4": //for left-handed mouse use
+				case "Numpad4":
+				case "Left": //IE/Edge specific value
+				case "ArrowLeft": cameraControls.moveRight(-moveMeters); break; //ambi/intuitive control
+				/* Fly right */
+				case "KeyD": //for right-handed mouse use
+				case "Digit6": //for left-handed mouse use
+				case "Numpad6":
+				case "Right": //IE/Edge specific value
+				case "ArrowRight": cameraControls.moveRight(moveMeters); break; //ambi/intuitive control
+				/* Fly forward */
+				case "KeyW": //for right-handed mouse use
+				case "Digit8": //for left-handed mouse use
+				case "Numpad8":
+				case "Up": //IE/Edge specific value
+				case "ArrowUp": cameraControls.moveForward(moveMeters); break; //ambi/intuitive control
+				/* Fly backward */
+				case "KeyS": //for right-handed mouse use
+				case "Digit5": //for left-handed mouse use
+				case "Numpad5":
+				case "Down": //IE/Edge specific value
+				case "ArrowDown": cameraControls.moveForward(-moveMeters); break; //ambi/intuitive control
+				/* Fly down */
+				case "KeyQ": //for right-handed mouse use
+				case "Digit7": //for left-handed mouse use
+				case "Numpad7":
+				case "PageDown": camera.position.y -= moveMeters; break; //ambi/intuitive control
+				/* Fly up */
+				case "KeyE": //for right-handed mouse use
+				case "Digit9": //for left-handed mouse use
+				case "Numpad9":
+				case "PageUp": camera.position.y += moveMeters; break; //ambi/intuitive control
+			}
+		};
+		/* Flight controls (mouse) */
+		const mouseWheelHandler = (e: WheelEvent) => {
+			const scaleFactor = 4;
+			if (e.deltaY > 0)
+				cameraControls.moveForward(-moveMeters * scaleFactor);
+			else
+				cameraControls.moveForward(moveMeters * scaleFactor);
+		}
+		/* Look controls */
+		// TBD
+		/* Add listeners */
+		renderer.domElement.addEventListener('mouseenter', mouseEnterVizAreaHandler, false);
+		renderer.domElement.addEventListener('mouseleave', mouseLeaveVizAreaHandler, false);
+		renderer.domElement.addEventListener('wheel', mouseWheelHandler, false);
+		/* Clean up old listeners if re-rendering */
+		return () => {
+			document.removeEventListener('keydown', keyPressHandler, false);
+			renderer.domElement.removeEventListener('mouseenter', mouseEnterVizAreaHandler, false);
+			renderer.domElement.removeEventListener('mouseleave', mouseLeaveVizAreaHandler, false);
+			renderer.domElement.removeEventListener('wheel', mouseWheelHandler, false);
+		};
+	}, [cameraControls,camera,renderer]);
 
 	const animationLoop = useCallback(() => {
 		renderer.render(scene, camera);
