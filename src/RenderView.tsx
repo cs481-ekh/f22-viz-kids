6dd64d565c2a09a5e21c9768bcd33f23f1b260d9
PTLux1517 <61641913+PTLux1517@users.noreply.github.com>
2022-10-05 17:46:44 -0600

    implement camera look controls (#108)
    * implement camera look controls
    * change look mouse button to middle click
        to avoid future overlap with marker selection

--- src/RenderView.tsx
+++ src/RenderView.tsx
@@ -130,17 +130,22 @@ export default function RenderView(props: Props) {
 				cameraControls.moveForward(moveMeters * scaleFactor);
 		}
 		/* Look controls */
-		// TBD
+		const mouseDownHandler = (e: MouseEvent) => {if (e.button===1) cameraControls.lock();}; //capture mouse movement (to rotate view) while holding middle click
+		const mouseUpHandler = (e: MouseEvent) => {if (e.button===1) cameraControls.unlock();}; //release mouse after middle click is over
 		/* Add listeners */
 		renderer.domElement.addEventListener('mouseenter', mouseEnterVizAreaHandler, false);
 		renderer.domElement.addEventListener('mouseleave', mouseLeaveVizAreaHandler, false);
 		renderer.domElement.addEventListener('wheel', mouseWheelHandler, false);
+		renderer.domElement.addEventListener('mousedown', mouseDownHandler, false);
+		renderer.domElement.addEventListener('mouseup', mouseUpHandler, false);
 		/* Clean up old listeners if re-rendering */
 		return () => {
 			document.removeEventListener('keydown', keyPressHandler, false);
 			renderer.domElement.removeEventListener('mouseenter', mouseEnterVizAreaHandler, false);
 			renderer.domElement.removeEventListener('mouseleave', mouseLeaveVizAreaHandler, false);
 			renderer.domElement.removeEventListener('wheel', mouseWheelHandler, false);
+			renderer.domElement.removeEventListener('mousedown', mouseDownHandler, false);
+			renderer.domElement.removeEventListener('mouseup', mouseUpHandler, false);
 		};
 	}, [cameraControls,camera,renderer]);
 
