960c6a26de0fd68c50823f9ad82fcb4323a1c7b0
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-10-06 18:17:57 -0600

    moved error popup styling into main stylesheet

--- src/ErrorPopup.tsx
+++ src/ErrorPopup.tsx
@@ -4,21 +4,8 @@ interface Props {
     error: Error|null;
 }
 
-const style: React.CSSProperties = {
-    zIndex: 2,
-    padding: 15,
-    whiteSpace: 'pre',
-    overflow: 'scroll',
-    color: '#AA0000',
-    backgroundColor: '#202020CC',
-    height: 420,
-    fontFamily: 'monospace, sans-serif',
-    fontSize: 12,
-    cursor: 'text'
-}
-
 export default function ErrorPopup(props: Props) {
-    return <div style={props.error ? {visibility: 'visible',...style} : {visibility: 'hidden',...style}}>
+    return <div id={"error-popup"} style={props.error ? {visibility: 'visible'} : {visibility: 'hidden'}}>
         {'Error: ' + props.error?.message}
     </div>
 }
