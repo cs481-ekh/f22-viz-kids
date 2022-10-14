42119860d8c1aaa71c3366c07b4db413b65749c6
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-10-13 20:12:05 -0600

    add simple angle calculation test
    to guide development of Calculations module computeAngle function

---
+++ src/Calculations.ts
@@ -0,0 +1,8 @@
+import { Point3D } from './DataTypes';
+
+
+/* Compute the angle between the first three Point3Ds in the provided Array,
+ * in degrees rounded to the nearest tenth, with index 0 as theta. */
+export function computeAngle(points: Array<Point3D|null>): number {
+    return 178.6;
+}
