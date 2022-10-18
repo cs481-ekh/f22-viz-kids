5d1d39293d5067dd3f1a8aa8218d1ef17c5ca1ca
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-10-18 16:14:42 -0600

    private work from personal repo clone

--- test/App.test.tsx
+++
@@ -1,7 +0,0 @@
-// import * as React from "react";
-// import { render, screen } from '@testing-library/react';
-// import App from '../src/App';
-
-test('dummy test', () => {
-  expect(true).toEqual(true);
-});
