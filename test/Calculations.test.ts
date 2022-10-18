29ad6f017c01758b2f22eb5085be6c98286e6376
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-10-18 15:47:42 -0600

    removed rounding to tenths
    this will be handled by the view instead

--- test/Calculations.test.ts
+++ test/Calculations.test.ts
@@ -6,5 +6,8 @@ test('valid angle (frame 0 left hip-knee-ankle angle)', () => {
     const LKJC: Point3D  = {x:0.10328, y:-1.47403, z:0.48009}; //left knee joint center
     const LASIS: Point3D = {x:0.07062, y:-1.31965, z:0.92865}; //left anterior-superior-iliac-spine (front edge of hip)
     const LAJC: Point3D  = {x:0.12071, y:-1.59442, z:0.12018}; //left ankle joint center
-    expect(computeAngle([LKJC,LASIS,LAJC])).toEqual(178.6); //calculated using https://onlinemschool.com/math/assistance/vector/angl/
+    const theta = computeAngle([LKJC,LASIS,LAJC]);
+    const expected = 178.600363111; //calculated using https://onlinemschool.com/math/assistance/vector/angl/
+    const digitsPrecision = 9;
+    expect(theta).toBeCloseTo(expected,digitsPrecision);
 });
