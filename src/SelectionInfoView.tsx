de3c943c93bfa2963dae2c0451dc3360b0e060c0
javitrejo14 <57146166+javitrejo14@users.noreply.github.com>
2022-10-25 23:12:35 -0600

    Implement marker tri-selection and display angle calculation  (#164)
    * Implement marker tri-selection
    
    Spent about 4 or more hours on this. Initially just understanding how to use teammate's new code.
    
    * Removed unnecessary import
    
    * Fixed requested changes
    
    Removed unnecessary white space as well as added needed white space. Fixed indentation
    
    * Another fix

--- src/SelectionInfoView.tsx
+++ src/SelectionInfoView.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import { MarkerFileData } from "./DataTypes";
 
+import { computeAngle} from "./Calculations";
+
 interface Props {
 	markerData: MarkerFileData;
 	selectedMarkers: number[];
@@ -9,26 +11,47 @@ interface Props {
 }
 
 export default function SelectionInfoView(props: Props) {
-	if(props.selectedMarkers.length === 0) return <p>Nothing selected.</p>;
-	return <div>
-		{
-			props.selectedMarkers.map(idx => {
-				const info = props.markerData.markers[idx];
-				const frameInfo = props.markerData.frames[props.frame].positions[idx];
+	if(props.selectedMarkers.length === 0)
+		return <div id={"selection-info-view"}><p>Nothing selected.</p></div>;
 
-				return <p key={idx}> {/* idx here is effectively an ID in the loaded data */}
-					Label: {info.label}<br />
-					{
-						frameInfo === null ?
-							"Unknown position" :
-							<>
-								x: {frameInfo.x}<br />
-								y: {frameInfo.y}<br />
-								z: {frameInfo.z}
-							</>
-					}
-				</p>;
-			})
+	const selectedMarkersMetadata = <>
+	{
+		props.selectedMarkers.map(idx => {
+			const info = props.markerData.markers[idx];
+			const frameInfo = props.markerData.frames[props.frame].positions[idx];
+
+			return <p key={idx}> {/* idx here is effectively an ID in the loaded data */}
+				Label: {info.label}<br />
+				{
+					frameInfo === null ?
+						"Unknown position" :
+						<>
+							x: {frameInfo.x}<br />
+							y: {frameInfo.y}<br />
+							z: {frameInfo.z}
+						</>
+				}
+			</p>;
+		})
+	}
+	</>;
+	const thetaLabel = props.markerData.markers[props.selectedMarkers[0]].label;
+	let angle: number|null = null;
+	let angleOutput;
+
+	if (props.selectedMarkers.length >= 3) {
+		const first3Pos = props.selectedMarkers.slice(0,3).map(idx => props.markerData.frames[props.frame].positions[idx]);
+		angle = computeAngle(first3Pos);
+		if (angle!==null) {
+			angle = Math.round(10 * angle) / 10; //round to nearest tenth
+			angleOutput = <p>{thetaLabel} Angle: {angle} deg</p>;
 		}
+		else angleOutput = <p>Unknown angle (missing marker)</p>;
+	}
+	else angleOutput = <></>; //no output: not enough selected
+
+	return <div id={"selection-info-view"}>
+		{angleOutput}
+		{selectedMarkersMetadata}
 	</div>;
 }
